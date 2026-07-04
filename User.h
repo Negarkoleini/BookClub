2abdbe0d773acd074564d1e84b821ac91bb4a874
fatemehoghyanosii <fatemehoghyanosi@email.com>
2026-07-04 10:34:47 +0430

    add full User class header

--- User.h
+++ User.h
@@ -1,4 +1,48 @@
 #ifndef USER_H
 #define USER_H
+#pragma once
+#include<string>
+enum class AccountStatus{
+    Active,
+    Blocked,
+    TemporarilyDisabled,
+};
+class User{
+private:
+    int id;
+    std::string username;
+    std::string passwordHash;
+    std::string email;
+    std::string roleString;
+    AccountStatus status;
+    std::string registrationDate;
+    std::string securityQuestion;
+    std::string securityAnswerHash;
+public:
+    User(int id, std::string username, std::string passwordHash, std::string email,
+        std::string roleString, std::string registrationDate);
+    virtual ~User()=default;
 
+    int getId()const;
+    void setId(const int &id);
+
+    std::string getUsername()const;
+    void setUsername(const std::string &uname);
+
+    std::string getPasswordHash()const;
+    void setPasswordHash(const std::string &pass);
+
+    std::string getEmail()const;
+    void setEmail(const std::string &email);
+
+    std::string getRoleString()const;
+    std::string getRegistrationDate() const;
+
+    AccountStatus getStatus() const;
+    void setStatus(AccountStatus s);
+
+    std::string getSecurityQuestion()const;
+    void setSecurityQuestion(const std::string &question,const std::string &answerHash);
+    bool verifySecurityAnswer(const std::string &answerHash)const;
+};
 #endif // USER_H
