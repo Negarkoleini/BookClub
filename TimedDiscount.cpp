f5411b2591ed2e862e38d47b170038a82e4368fc
fatemehoghyanosii <fatemehoghyanosi@email.com>
2026-07-05 07:50:21 +0430

    add source TimedDiscount class

---
+++ TimedDiscount.cpp
@@ -0,0 +1,21 @@
+#include "TimedDiscount.h"
+
+TimedDiscount::TimedDiscount(int targetBookId, double discountPercentage, std::string startDateTime, std::string endDateTime):
+targetBookId(targetBookId),
+discountPercentage(discountPercentage>=0.0 && discountPercentage<=100?discountPercentage:0.0),
+startDateTime(startDateTime),
+endDateTime(endDateTime){}
+
+int TimedDiscount::getTargetBookId() const{
+    return targetBookId;
+}
+double TimedDiscount::getDiscountPercentage() const{
+    return discountPercentage;
+}
+bool TimedDiscount::isActiveNow(const std::string &currentSystemTime) const{
+    return currentSystemTime>=startDateTime && currentSystemTime<=endDateTime;
+}
+double TimedDiscount::getDiscountedPrice(double originalPrice) const{
+    double discountamount=originalPrice*(discountPercentage/100.0);
+    return originalPrice-discountamount;
+}
